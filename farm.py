91af03f0c504c4355bb732873ac3d5a1f295c8a3
Bogorosh Evgeny <bogorosh@mail.ru>
2020-06-20 20:32:26 +0300

    PyCharm commit 2

--- farm.py
+++ farm.py
@@ -1,24 +1,42 @@
-class Goose:
-    pass
+from pprint import pprint
+
+class Animals:
+    name = 'Name'
+    weight = 0
+    condition = 'hungry'
+
+    def __init__(self):
+        self.feed = 'fed'
 
 
-class Cow:
-    pass
 
+goose0 = Animals()
+goose0.name = 'Gray'
+goose0.weight = 4 #kg
+
+goose1 = Animals()
+goose1.name = 'White'
+goose1.weight = 3 #kg
+
+
+Cow = Animals()
 
 class Sheep:
     pass
 
-
 class Chicken:
     pass
-
 
 class Goat:
     pass
 
-
 class Duck:
     pass
+
+pprint(Animals.__dict__)
+print(goose0.__dict__)
+print(goose1.__dict__)
+
+
 
 
